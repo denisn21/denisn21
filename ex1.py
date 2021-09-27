192d8958b609cd6f6cbcae8279ca15ecab1c78dc
denisn21 <70665217+denisn21@users.noreply.github.com>
2021-09-27 18:05:56 +0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#ask user his name and age, and tell him when he will reach 100 years
+
+
+
+name=input('Tell me your name: ')
+print('Your name is' + ' ' + name)
+
+age=int(input('Tell me your age: '))
+print('You will turn 100 in' + ' ' + str(2021+100-age))
